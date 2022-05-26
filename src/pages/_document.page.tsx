581b97990c79532d086ce1c4f69e307ac84ba7a7
sdmilieu <sdmilieu@gmail.com>
2022-05-26 15:09:12 +0900

    htmlのlang属性をjaにしてポップアップが出てこないように

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded&display=swap"
