4e3281132e6ead9b2b1b669482b13f9d269dc63d
sdmilieu <sdmilieu@gmail.com>
2022-06-06 14:38:35 +0900

    自動生成されるファイルもGit管理するように

---
+++ src/.pathpida/$path.ts
@@ -0,0 +1,16 @@
+import type { Query as Query0 } from '../pages/my_recipe/detail.page'
+
+export const pagesPath = {
+  "my_recipe": {
+    "create": {
+      $url: (url?: { hash?: string }) => ({ pathname: '/my_recipe/create' as const, hash: url?.hash })
+    },
+    "detail": {
+      $url: (url: { query: Query0, hash?: string }) => ({ pathname: '/my_recipe/detail' as const, query: url.query, hash: url.hash })
+    },
+    $url: (url?: { hash?: string }) => ({ pathname: '/my_recipe' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+export type PagesPath = typeof pagesPath
